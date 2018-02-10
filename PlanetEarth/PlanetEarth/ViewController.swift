0ca10ecd3a8884832ea13c6e919eb50776711ebe
Ronin Winter <roninwinter@gmail.com>
2018-02-10 16:36:12 +0700

    Intial Commit

---
+++ PlanetEarth/PlanetEarth/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  PlanetEarth
+//
+//  Created by Ronin Winter on 2/2/18.
+//  Copyright © 2018 roninwinter. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var sceneView: ARSCNView!
+    let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin, ARSCNDebugOptions.showFeaturePoints]
+          self.sceneView.session.run(configuration)
+          self.sceneView.autoenablesDefaultLighting = true 
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        let earth = SCNNode()
+        earth.geometry = SCNSphere(radius: 0.2)
+        earth.geometry?.firstMaterial?.diffuse.contents = #imageLiteral(resourceName: "Earth day")
+        earth.geometry?.firstMaterial?.specular.contents = #imageLiteral(resourceName: "Earth Specular")
+        earth.geometry?.firstMaterial?.emission.contents = #imageLiteral(resourceName: "Earth Emission")
+        earth.geometry?.firstMaterial?.normal.contents = #imageLiteral(resourceName: "Earth Normal")
+        earth.position = SCNVector3(0,0,-1)
+        self.sceneView.scene.rootNode.addChildNode(earth)
+        
+        let action = SCNAction.rotateBy(x: 0, y: CGFloat(360.degreesToRadians), z: 0, duration: 8)
+        let forever = SCNAction.repeatForever(action)
+        earth.runAction(forever )
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
+}
+
